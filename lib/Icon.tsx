dba9464bef4a4dc26790f839842880cc314e11f5
guosenquan <guosenquan@supcon.com>
2021-05-21 08:42:48 +0800

    对icon参数的解构赋值 简化参数

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -8,11 +8,10 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const {className,...restProps} = props;
+const Icon: React.FunctionComponent<IconProps> = ({ className, name, ...restProps }) => {
   return (
-    <svg className={classes('gsq-icon',className)} {...restProps}>
-      <use xlinkHref={`#${props.name}`} />
+    <svg className={classes('gsq-icon', className)} {...restProps}>
+      <use xlinkHref={`#${name}`} />
     </svg>
   );
 };
