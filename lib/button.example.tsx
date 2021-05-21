38d16fe55d1a22c4fe898c68d917668d75d09b3e
guosenquan <guosenquan@supcon.com>
2021-05-21 16:20:57 +0800

    add example

---
+++ lib/button.example.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ButtonExample: React.FunctionComponent = () => {
+  return (
+    
+    <div>{console.log('button')}hi im button</div>
+  );
+}
+
+export default ButtonExample;
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from './icon'
+
+const IconExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Icon name='user'/>
+      <Icon name='inventory'/>
+      <Icon name='trend'/>
+    </div>
+  )
+}
+export default IconExample
