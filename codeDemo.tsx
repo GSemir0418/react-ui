29dcd61629a2329b7b7069b80961265b5468f4b0
guosenquan <guosenquan@supcon.com>
2021-05-25 17:32:57 +0800

    codedemo语法高亮

--- codeDemo.tsx
+++ codeDemo.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HighLight, { defaultProps } from 'prism-react-renderer'
 
 interface Props {
   code: string
 }
 
 const CodeDemo: React.FunctionComponent<Props> = (props) => {
+  const [codeVisible, setCodeVisible] = useState(false)
   return (
     <div>
-      {props.children}
-      <pre>
-        {props.code}
-      </pre>
+      <div>
+        {props.children}
+      </div>
+      <div>
+        <button onClick={() => setCodeVisible(!codeVisible)}>查看代码</button>
+      </div>
+      <HighLight {...defaultProps} code={props.code} language='jsx'>
+        {
+          ({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )
+        }
+      </HighLight>
     </div>
   )
 }
